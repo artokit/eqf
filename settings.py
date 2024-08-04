9d1bb2155f6192702178cc2b749da1ca0cb8b833
artokit <kdsa3102@gmail.com>
2024-08-04 23:33:35 +0300

    i

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import os
 
 MEDIA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media')
-TOKEN = "6505579504:AAFinIn8TWYt2M43i4FcfArvbf4o_hmOrRY"
-ME_LINK = "https://t.me/Eduardo_Win"
+TOKEN = "6742630387:AAHV9IvAizFJc8OtaQcAE0SFLEw45W6CETg"
+ME_LINK = "https://t.me/El_Mismo_Eduardo"
